2f3f6dbc0c749d24e4b6ce0feeaa85f852993536
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-10-09 15:30:53 -0400

    Added fragment to Activity Portrait

---
+++ app/src/main/java/com/remijonathan/carwash3/CalculatePriceFragment.java
@@ -0,0 +1,28 @@
+package com.remijonathan.carwash3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CalculatePriceFragment extends Fragment {
+
+    public CalculatePriceFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calculate_price, container, false);
+    }
+}
