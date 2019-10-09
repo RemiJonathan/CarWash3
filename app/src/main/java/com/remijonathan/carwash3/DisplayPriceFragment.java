58884ca98e5921ed79bad98ce177329e038c441a
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-10-09 15:41:18 -0400

    Added Display Fragment to Landscape orientation

---
+++ app/src/main/java/com/remijonathan/carwash3/DisplayPriceFragment.java
@@ -0,0 +1,28 @@
+package com.remijonathan.carwash3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DisplayPriceFragment extends Fragment {
+
+    public DisplayPriceFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_display_price, container, false);
+    }
+}
