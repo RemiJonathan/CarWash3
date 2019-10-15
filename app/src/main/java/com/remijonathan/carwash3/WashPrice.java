7541f9bede0921814953649e881c5cba30420a41
RemiJonathan <contact@remijonathan.com>
2019-10-15 00:11:18 -0400

    Update WashPrice.java

--- app/src/main/java/com/remijonathan/carwash3/WashPrice.java
+++ app/src/main/java/com/remijonathan/carwash3/WashPrice.java
@@ -3,12 +3,17 @@ package com.remijonathan.carwash3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WashPrice extends AppCompatActivity {
+    private TextView totalPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wash_price);
+        totalPrice = findViewById(R.id.total_price);
+
+        totalPrice.setText(getIntent().getStringExtra("Price"));
     }
 }
