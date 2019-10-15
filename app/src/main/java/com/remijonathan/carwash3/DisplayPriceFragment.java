cb56858074fc935c07a6437c4b0f93f8e5200d14
Rémi Jonathan Choquette <contact@remijonathan.com>
2019-10-15 13:34:00 -0400

    It should Work

--- app/src/main/java/com/remijonathan/carwash3/DisplayPriceFragment.java
+++ app/src/main/java/com/remijonathan/carwash3/DisplayPriceFragment.java
@@ -7,12 +7,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.zip.Inflater;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class DisplayPriceFragment extends Fragment {
+
+    private TextView price;
 
     public DisplayPriceFragment() {
         // Required empty public constructor
@@ -22,7 +27,13 @@ public class DisplayPriceFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_display_price,container,false);
+        price = view.findViewById(R.id.total_price);
+
+        price.setText(getArguments().getString("Price"));
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display_price, container, false);
+        return view;
     }
 }
